3f474b724757fd84fbddbdc61f72450d2ebcab58
mkepes (Matej Kepes) <matejkepes@gmail.com>
2018-08-14 00:53:31 +0100

    Add test for create_item function

--- tests/tests.c
+++ tests/tests.c
@@ -5,6 +5,7 @@
 #include <cmocka.h>
 #include <string.h>
 #include "command.h"
+#include "item.h"
 
 static void createCommand_noMatches_returnsCommandsWithGroupsEqualsNull(void ** state)
 {
@@ -39,12 +40,23 @@ static void test_destroy_command(void ** state)
     assert_true(command->description == NULL);
 }   
 
+
+static void createItem_allocatedProperly(void ** state) 
+{
+    ITEM * item = create_item("SWORD", "MIGHTY SWORD", 0x0000);
+
+    assert_true(strcmp(item->name, "SWORD") == 0);
+    assert_true(strcmp(item->description, "MIGHTY SWORD") == 0);
+    assert_true(item->properties == 0x0000);
+}
+
 int main(void) 
 {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(createCommand_noMatches_returnsCommandsWithGroupsEqualsNull),
         cmocka_unit_test(createCommand_withMatches_returnsCommandsWithGroupsNotNull),
-        cmocka_unit_test(test_destroy_command)
+        cmocka_unit_test(test_destroy_command),
+        cmocka_unit_test(createItem_allocatedProperly)
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
