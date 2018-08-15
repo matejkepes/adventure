5f5bc6730a273a53a6ac2ecc7d46e486d312f0a7
mkepes (Matej Kepes) <matejkepes@gmail.com>
2018-08-16 00:02:23 +0100

    Add Unit tests for Backpack

--- tests/tests.c
+++ tests/tests.c
@@ -8,6 +8,7 @@
 #include "command.h"
 #include "item.h"
 #include "container.h"
+#include "backpack.h"
 
 static void createCommand_noMatches_returnsCommandsWithGroupsEqualsNull(void **state)
 {
@@ -238,7 +239,7 @@ static void removeContainer__EntryInListListOfSizeOne_returnsNull(void **state)
     assert_null(remove_container(first, itemOne));
 }
 
-static void removeContainer_EntryInMiddle_ReturnsReferenceListChanged(void ** state) 
+static void removeContainer_EntryInMiddle_ReturnsReferenceListChanged(void **state)
 {
     ITEM *itemOne = create_item("ITEM_ONE", "MIGHTY SWORD", 0x0000);
     ITEM *itemTwo = create_item("ITEM_TWO", "MIGHTY SWORD", 0x0000);
@@ -248,19 +249,13 @@ static void removeContainer_EntryInMiddle_ReturnsReferenceListChanged(void ** st
     struct container *second = create_container(first, TYPE_ITEM, itemTwo);
     struct container *third = create_container(second, TYPE_ITEM, itemThree);
 
-    struct container * afterItemInTheMiddleRemoved = remove_container(third, itemTwo);
+    struct container *afterItemInTheMiddleRemoved = remove_container(third, itemTwo);
 
     assert_int_equal(2, get_linked_list_size(afterItemInTheMiddleRemoved));
+    assert_null(second->next);
     assert_ptr_equal(third, get_nth_item_from_linked_list(afterItemInTheMiddleRemoved, 0));
     assert_ptr_equal(first, get_nth_item_from_linked_list(afterItemInTheMiddleRemoved, 1));
 }
-
-//list empty test, entry empty
-//list empty test, entry not empty
-
-//list not empty, entry empty
-//list not empty, entry not empty
-//
 
 static void removeFromContainer_ListNotEmpty_ItemRemovedProperly(void **state)
 {
@@ -274,27 +269,188 @@ static void removeFromContainer_ListNotEmpty_ItemRemovedProperly(void **state)
     struct container *third = create_container(second, TYPE_ITEM, itemThree);
 }
 
+static void backpack_create_backpack(void **state)
+{
+    const int BACKPACK_CAPACITY = 10;
+    BACKPACK *backpack = create_backpack(BACKPACK_CAPACITY);
+
+    assert_non_null(backpack);
+    assert_int_equal(0, backpack->size);
+    assert_int_equal(BACKPACK_CAPACITY, backpack->capacity);
+    assert_null(backpack->items);
+}
+
+static void backpack_destroy_backpack_backpackNullReturnsNull(void **state)
+{
+    assert_null(destroy_backpack(NULL));
+}
+
+static void backpack_destroy_backpack(void **state)
+{
+    const int BACKPACK_CAPACITY = 10;
+    BACKPACK *backpack = create_backpack(BACKPACK_CAPACITY);
+
+    //Setup list of three items
+    ITEM *itemOne = create_item("ITEM_ONE", "MIGHTY SWORD", 0x0000);
+    ITEM *itemTwo = create_item("ITEM_TWO", "MIGHTY SWORD", 0x0000);
+    ITEM *itemThree = create_item("ITEM_THREE", "MIGHTY SWORD", 0x0000);
+    struct container *first = create_container(NULL, TYPE_ITEM, itemOne);
+    struct container *second = create_container(first, TYPE_ITEM, itemTwo);
+    struct container *third = create_container(second, TYPE_ITEM, itemThree);
+
+    backpack->items = third;
+    backpack->size = 3;
+
+    //Essentially !!! destroy_containers must be called
+    assert_null(destroy_backpack(backpack));
+    assert_null(itemOne->name);
+    assert_null(itemTwo->name);
+    assert_null(itemThree->name);
+    assert_null(first->item);
+    assert_null(second->item);
+    assert_null(third->item);
+}
+
+static void backpack_add_item_to_backpack_returnsFalseWhenCapacity0(void **state)
+{
+    const int BACKPACK_CAPACITY = 0;
+    BACKPACK *backpack = create_backpack(BACKPACK_CAPACITY);
+    ITEM *itemOne = create_item("ITEM_ONE", "MIGHTY SWORD", 0x0000);
+
+    assert_false(add_item_to_backpack(backpack, itemOne));
+    assert_null(backpack->items);
+    assert_int_equal(0, backpack->size);
+}
+
+static void backpack_add_item_to_backpack_returnsTrueWhenCapacityIsNotExceeded(void **state)
+{
+    const int BACKPACK_CAPACITY = 1;
+    BACKPACK *backpack = create_backpack(BACKPACK_CAPACITY);
+    ITEM *itemOne = create_item("ITEM_ONE", "MIGHTY SWORD", 0x0000);
+
+    assert_true(add_item_to_backpack(backpack, itemOne));
+    assert_ptr_equal(backpack->items->item, itemOne);
+    assert_int_equal(1, backpack->size);
+}
+
+static void backpack_add_item_to_backpack_returnsFalseWhenMaxCapacityReached(void **state)
+{
+    const int BACKPACK_CAPACITY = 1;
+    BACKPACK *backpack = create_backpack(BACKPACK_CAPACITY);
+    ITEM *itemOne = create_item("ITEM_ONE", "MIGHTY SWORD", 0x0000);
+    ITEM *itemTwo = create_item("ITEM_TWO", "MIGHTY SWORD", 0x0000);
+
+    assert_true(add_item_to_backpack(backpack, itemOne));
+    assert_false(add_item_to_backpack(backpack, itemTwo));
+    assert_ptr_equal(backpack->items->item, itemOne);
+    assert_int_equal(1, backpack->size);
+}
+
+static void backpack_delete_item_from_backpack_backpackNull_DoesntCrash(void **state)
+{
+    delete_item_from_backpack(NULL, NULL);
+}
+
+static void backpack_delete_item_from_backpack_ItemNull_DoesntCrash(void **state)
+{
+    const int BACKPACK_CAPACITY = 1;
+    BACKPACK *backpack = create_backpack(BACKPACK_CAPACITY);
+
+    delete_item_from_backpack(backpack, NULL);
+}
+
+static void backpack_delete_item_from_backpack_ItemRemovedWhenInBackpack(void **state)
+{
+    const int BACKPACK_CAPACITY = 1;
+    ITEM *itemOne = create_item("ITEM_ONE", "MIGHTY SWORD", 0x0000);
+    BACKPACK *backpack = create_backpack(BACKPACK_CAPACITY);
+    backpack->size = 1;
+    backpack->capacity = BACKPACK_CAPACITY;
+    backpack->items = create_container(NULL, TYPE_ITEM, itemOne);
+
+    delete_item_from_backpack(backpack, itemOne);
+    assert_int_equal(backpack->size, 0);
+    assert_int_equal(backpack->capacity, BACKPACK_CAPACITY);
+    assert_null(backpack->items);
+}
+
+static void backpack_delete_item_from_backpack_ItemNotRemoved_WhenNotInBackpack(void **state)
+{
+    const int BACKPACK_CAPACITY = 1;
+    ITEM *itemOne = create_item("ITEM_ONE", "MIGHTY SWORD", 0x0000);
+    ITEM *itemTwo = create_item("ITEM_TWO", "MIGHTY SWORD", 0x0000);
+
+    BACKPACK *backpack = create_backpack(BACKPACK_CAPACITY);
+    backpack->size = 1;
+    backpack->capacity = BACKPACK_CAPACITY;
+    backpack->items = create_container(NULL, TYPE_ITEM, itemOne);
+
+    //Delete an item which is not in the backpack
+    delete_item_from_backpack(backpack, itemTwo);
+
+    assert_int_equal(backpack->size, 1);
+    assert_int_equal(backpack->capacity, BACKPACK_CAPACITY);
+    assert_ptr_equal(backpack->items, itemOne);
+}
+
+static void backpack_get_item_backpackNull_returnsNull(void **state)
+{
+}
+
+static void backpack_get_item_ItemNotInBackpack_returnsNull(void **state)
+{
+}
+
+static void backpack_get_item_ItemInBackpack_ItemReturnedBackpackItemsIntact(void **state)
+{
+}
+
 int main(void)
 {
     const struct CMUnitTest tests[] = {
+        //Command - Create
         cmocka_unit_test(createCommand_noMatches_returnsCommandsWithGroupsEqualsNull),
         cmocka_unit_test(createCommand_withMatches_returnsCommandsWithGroupsNotNull),
+        //Command - Destroy
         cmocka_unit_test(test_destroy_command),
+        //Item - Create
         cmocka_unit_test(createItem_allocatedProperly),
+        //Container - Create
         cmocka_unit_test(createContainer_firstItemNull_returnsNewContainer),
         cmocka_unit_test(createContainer_firstItemNotNull_returnsNewContainerItemIsNewHead),
+        //Container - Destroy
         cmocka_unit_test(destroyContainers_NULLAsFirst_returnsNull),
         cmocka_unit_test(destroyContainers_FirstItemPassedIn_returnsNullOBjectsFreed),
+        //Container - Get item from container
         cmocka_unit_test(getFromContainer_ListNullReturnsNull),
         cmocka_unit_test(getFromContainer_ListContainsItem_ItemRetrieved),
+        //Container - Remove container
         cmocka_unit_test(removeContainer__ListNullEntryNull__returnsNull),
         cmocka_unit_test(removeContainer__ListNullEntryNotNull__returnsNull),
         cmocka_unit_test(removeContainer__EntryNull__returnsUnchagedList),
         cmocka_unit_test(removeContainer__EntryNotInList_returnsReferenceListUnchanged),
         cmocka_unit_test(removeContainer__EntryInList__returnsReferenceListChanged),
         cmocka_unit_test(removeContainer__EntryInListListOfSizeOne_returnsNull),
-        cmocka_unit_test(removeContainer_EntryInMiddle_ReturnsReferenceListChanged)
-        };
+        cmocka_unit_test(removeContainer_EntryInMiddle_ReturnsReferenceListChanged),
+        //Backpack - Create
+        cmocka_unit_test(backpack_create_backpack),
+        //Backpack - Destroy
+        cmocka_unit_test(backpack_destroy_backpack),
+        cmocka_unit_test(backpack_destroy_backpack_backpackNullReturnsNull),
+        //Backpack - Add item
+        cmocka_unit_test(backpack_add_item_to_backpack_returnsFalseWhenCapacity0),
+        cmocka_unit_test(backpack_add_item_to_backpack_returnsTrueWhenCapacityIsNotExceeded),
+        cmocka_unit_test(backpack_add_item_to_backpack_returnsFalseWhenMaxCapacityReached),
+        //Backpack - Delete item
+        cmocka_unit_test(backpack_delete_item_from_backpack_backpackNull_DoesntCrash),
+        cmocka_unit_test(backpack_delete_item_from_backpack_ItemNull_DoesntCrash),
+        cmocka_unit_test(backpack_delete_item_from_backpack_ItemRemovedWhenInBackpack),
+        cmocka_unit_test(backpack_delete_item_from_backpack_ItemNotRemoved_WhenNotInBackpack),
+        //Backpack - Get item
+        cmocka_unit_test(backpack_get_item_backpackNull_returnsNull),
+        cmocka_unit_test(backpack_get_item_ItemNotInBackpack_returnsNull),
+        cmocka_unit_test(backpack_get_item_ItemInBackpack_ItemReturnedBackpackItemsIntact),
 
+    };
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
