f1c7f116da1aa28420b033a58000126cc3436896
Veronika Rusinkova <pr17vr@leeds.ac.uk>
2018-08-18 00:19:57 +0100

    Add add_room_to_world

--- src/world.c
+++ src/world.c
@@ -11,6 +11,22 @@ struct container *create_world()
 
 struct container *add_room_to_world(struct container *world, struct room *room)
 {
+    if (world == NULL)
+    {
+        return NULL;
+    }
+
+    if (room == NULL)
+    {
+        return world;
+    }
+
+    if (!get_from_container_by_name(world, room->name))
+    {
+        world = create_container(world, TYPE_ROOM, room);
+    }
+
+    return world;
 }
 
 struct container *destroy_world(struct container *world)
