51f0ed00e52bb8be5a426b84c22a36c92494d012
Veronika Rusinkova <pr17vr@leeds.ac.uk>
2018-08-17 23:08:46 +0100

    Add create_world()

--- src/world.c
+++ src/world.c
@@ -1,16 +1,22 @@
-struct container* create_world() 
-{
+#include "container.h"
 
-}
-struct container* add_room_to_world(struct container* world, struct room* room)
+struct container *create_world()
 {
+    struct container *world = create_container(world->room, TYPE_ROOM, NULL);
+    world->item = NULL;
+    world->room = NULL;
+    world->command = NULL;
+    world->text = NULL;
+}
 
-}
-struct container* destroy_world(struct container* world)
+struct container *add_room_to_world(struct container *world, struct room *room)
 {
+}
 
-}
-struct room* get_room(struct container* world, char* name)
+struct container *destroy_world(struct container *world)
 {
-    
+}
+
+struct room *get_room(struct container *world, char *name)
+{
 }
