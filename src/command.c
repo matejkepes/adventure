5d5b9bf27f02b4f04e077a322deb0d674ec36e85
mkepes (Matej Kepes) <matejkepes@gmail.com>
2018-08-19 00:46:25 +0100

    Create parser with all commands, segfault fixes

--- src/command.c
+++ src/command.c
@@ -4,38 +4,45 @@
 #include <string.h>
 #include <stddef.h>
 
-COMMAND *create_command (char *name, char *description, char *pattern, size_t nmatch) {
+COMMAND *create_command(char *name, char *description, char *pattern, size_t nmatch)
+{
     //Alloc for command struct
     COMMAND *command = malloc(sizeof(COMMAND));
-   
+
     //Alloc for command->name
     command->name = strdup(name);
-   
+
     //Alloc for command->description
     command->description = strdup(description);
-   
+
     //Compile regex_t preg
     int compilation_result = regcomp(&(command->preg), pattern, REG_EXTENDED | REG_ICASE);
-    if (compilation_result) {
+    if (compilation_result)
+    {
         printf("Could not compile regex\n");
         return NULL;
-    }   
+    }
 
     command->nmatch = nmatch;
     command->groups = NULL;
 
-    if(nmatch > 0) {
-        command->groups = (char ** ) malloc(sizeof(char * ) * nmatch);
+    if (nmatch > 0)
+    {
+        command->groups = (char **)malloc(sizeof(char *) * nmatch);
+
+        for(int i = 0; i < nmatch; i++) 
+            command->groups[i] = NULL;
     }
 
     return command;
 }
 
-COMMAND *destroy_command (COMMAND *command) {
+COMMAND *destroy_command(COMMAND *command)
+{
     //free name
     free(command->name);
     command->name = NULL;
-    
+
     //free description
     free(command->description);
     command->description = NULL;
@@ -44,11 +51,14 @@ COMMAND *destroy_command (COMMAND *command) {
     regfree(&(command->preg));
 
     //free all groups
-    for (int i=0; i<command->nmatch; i++) {
-        free(command->groups[i]);
+    for (int i = 0; i < command->nmatch; i++)
+    {
+        if(command->groups[i] != NULL)        
+            free(command->groups[i]);
     }
 
-    free(command->groups);
+    if(command->groups != NULL)
+        free(command->groups);
 
     return NULL;
 }
--- src/parser.c
+++ src/parser.c
@@ -5,40 +5,54 @@
 
 struct parser *create_parser()
 {
-    // Quit       |  Exit, Q
-    // North      |  N
-    // South      |  S
-    // East       |  E
-    // West       |  W
-    // Look       |  
-    // Help       |  H
-    // Version    |
-    // Restart    |
-    // About      |
-    // Take       |
-    // Drop       |
-    // Inventory  |  I
-    // Use        |
-    // Examine    |
-    // Load       |
-    // Save       |
-
-
-
-
-
-
-
-
-
-
-
-    // struct container *commands = create_command("look", "scan your environment for objects", "/look/gi", );
-
     struct parser *parser = malloc(sizeof(struct parser));
 
     if (parser != NULL)
+    {
+        COMMAND *north = create_command("North", "FILL IN DESC", "^\\ *(north|n)\\ *$", 0);
+        COMMAND *south = create_command("South", "FILL IN DESC", "^\\ *(south|s)\\ *$", 0);
+        COMMAND *east = create_command("East", "FILL IN DESC", "^\\ *(east|s)\\ *$", 0);
+        COMMAND *west = create_command("West", "FILL IN DESC", "^\\ *(west|s)\\ *$", 0);
+        COMMAND *look = create_command("Look", "FILL IN DESC", "^\\ *look\\ *$", 0);
+
+        COMMAND *take = create_command("Take", "FILL IN DESC", "^\\ *take\\ +([a-z0-9][a-z0-9\\ ]*[a-z0-9])\\ *$", 1);
+        COMMAND *drop = create_command("Drop", "FILL IN DESC", "^\\ *drop\\ +([a-z0-9][a-z0-9\\ ]*[a-z0-9])\\ *$", 1);
+        COMMAND *use = create_command("Use", "FILL IN DESC", "^\\ *use\\ +([a-z0-9][a-z0-9\\ ]*[a-z0-9])\\ *$", 1);
+        COMMAND *examine = create_command("Examine", "FILL IN DESC", "^\\ *examine\\ +([a-z0-9][a-z0-9\\ ]*[a-z0-9])\\ *$", 1);
+
+        COMMAND *inventory = create_command("Inventory", "FILL IN DESC", "^\\ *(inventory|i)\\ *$", 0);
+
+        COMMAND *restart = create_command("Restart", "FILL IN DESC", "^\\ *restart\\ *$", 0);
+        COMMAND *quit = create_command("Quit", "FILL IN DESC", "^\\ *(quit|exit|q)\\ *$", 0);
+        COMMAND *load = create_command("Load", "FILL IN DESC", "^\\ *load\\ *$", 0);
+        COMMAND *save = create_command("Save", "FILL IN DESC", "^\\ *save\\ *$", 0);
+        COMMAND *version = create_command("Version", "FILL IN DESC", "^\\ *version\\ *$", 0);
+        COMMAND *about = create_command("About", "FILL IN DESC", "^\\ *about\\ *$", 0);
+        COMMAND *help = create_command("Help", "FILL IN DESC", "^\\ *(help|h)\\ *$", 0);
+
+        struct container *commands = create_container(NULL, TYPE_COMMAND, north);
+        commands = create_container(commands, TYPE_COMMAND, south);
+        commands = create_container(commands, TYPE_COMMAND, east);
+        commands = create_container(commands, TYPE_COMMAND, west);
+        commands = create_container(commands, TYPE_COMMAND, look);
+        commands = create_container(commands, TYPE_COMMAND, take);
+        commands = create_container(commands, TYPE_COMMAND, drop);
+        commands = create_container(commands, TYPE_COMMAND, use);
+        commands = create_container(commands, TYPE_COMMAND, examine);
+        commands = create_container(commands, TYPE_COMMAND, inventory);
+
+        commands = create_container(commands, TYPE_COMMAND, restart);
+        commands = create_container(commands, TYPE_COMMAND, quit);
+        commands = create_container(commands, TYPE_COMMAND, load);
+        commands = create_container(commands, TYPE_COMMAND, save);
+        commands = create_container(commands, TYPE_COMMAND, version);
+        commands = create_container(commands, TYPE_COMMAND, about);
+        commands = create_container(commands, TYPE_COMMAND, help);
+
+        parser->commands = commands;
+        parser->history = NULL;
         return parser;
+    }
 
     return NULL;
 }
