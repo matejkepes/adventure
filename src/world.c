feb772b5cfa350df5d8d418d5e47f6249cd0b17d
Veronika Rusinkova <pr17vr@leeds.ac.uk>
2018-08-18 01:15:30 +0100

    Add destroy_world_()

--- src/world.c
+++ src/world.c
@@ -22,6 +22,8 @@ struct container *add_room_to_world(struct container *world, struct room *room)
 
 struct container *destroy_world(struct container *world)
 {
+    world = destroy_containers(world);
+    return NULL;
 }
 
 struct room *get_room(struct container *world, char *name)
