8a68d952ccafd2bc66bc45a3c68a641815fd27d2
Veronika Rusinkova <pr17vr@leeds.ac.uk>
2018-08-18 17:59:33 +0100

    Add destroy_parser()

--- src/parser.c
+++ src/parser.c
@@ -2,16 +2,30 @@
 #include <regex.h>
 #include "parser.h"
 
-struct parser* create_parser()
+struct parser *create_parser()
 {
+    // struct container *commands = create_command("look", "scan your environment for objects", "/(look)/gi", );
 
+    struct parser *parser = malloc(sizeof(struct parser));
+
+    if (parser != NULL)
+        return parser;
+
+    return NULL;
 }
 
-struct parser* destroy_parser(struct parser * parser)
+struct parser *destroy_parser(struct parser *parser)
 {
+    if (parser != NULL) {
+        parser = destroy_containers(parser->commands);
+        parser = destroy_containers(parser->history);
+        parser = NULL;
+    }
 
+    return NULL;
 }
 
-struct command* parse_input(struct parser* parser, char* input) 
+struct command *parse_input(struct parser *parser, char *input)
 {
+
 }
