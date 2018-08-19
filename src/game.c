8a92df1beccef8d2725c87791a6b4b6ca1f1de3a
Veronika Rusinkova <pr17vr@leeds.ac.uk>
2018-08-19 20:48:49 +0100

    Add 'Take' command to execute_command()

--- src/game.c
+++ src/game.c
@@ -61,4 +61,89 @@ struct game *destroy_game(struct game *game)
 
 void execute_command(struct game *game, struct command *command)
 {
+    if (game == NULL || command == NULL)
+        return;
+
+    if (strcmp(command->name, "North") == 0)
+    {
+    }
+
+    if (strcmp(command->name, "South") == 0)
+    {
+    }
+
+    if (strcmp(command->name, "East") == 0)
+    {
+    }
+
+    if (strcmp(command->name, "West") == 0)
+    {
+    }
+
+    if (strcmp(command->name, "Look") == 0)
+    {
+        show_room(game->current_room);
+    }
+
+    if (strcmp(command->name, "Take") == 0)
+    {
+        struct container *item_to_take = get_from_container_by_name(game->current_room->items, command->groups);
+    
+        if (item_to_take == NULL)
+            ("There is no such item in this room.\n");
+        
+        if (item_to_take->item->properties != MOVABLE)
+            ("This item is too heavy to be picked up.\n");
+
+        if (game->backpack->size >= game->backpack->capacity)
+            ("There is not enough room in your backpack for this item. You will have to drop something else first.\n");
+    
+        game->backpack = item_to_take;
+        game->current_room->items = remove_container(game->current_room->items, item_to_take);
+        printf("You have taken %s.\n", item_to_take);
+    }
+
+    if (strcmp(command->name, "Drop") == 0)
+    {
+    }
+
+    if (strcmp(command->name, "Use") == 0)
+    {
+    }
+
+    if (strcmp(command->name, "Examine") == 0)
+    {
+    }
+
+    if (strcmp(command->name, "Inventory") == 0)
+    {
+    }
+
+    if (strcmp(command->name, "Restart") == 0)
+    {
+    }
+
+    if (strcmp(command->name, "Quit") == 0)
+    {
+    }
+
+    if (strcmp(command->name, "Load") == 0)
+    {
+    }
+
+    if (strcmp(command->name, "Save") == 0)
+    {
+    }
+
+    if (strcmp(command->name, "Version") == 0)
+    {
+    }
+
+    if (strcmp(command->name, "About") == 0)
+    {
+    }
+
+    if (strcmp(command->name, "Help") == 0)
+    {
+    }
 }
